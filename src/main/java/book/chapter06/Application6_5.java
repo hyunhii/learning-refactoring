ab822605e5f002f65432c9b99a3364d99d4713e7
hyunhii <be_hyunny@naver.com>
2023-10-30 18:57:35 +0900

    refactor: 매개변수를 속성으로 바꾸기 - anyMatch()에서 contains()로 변경

--- src/main/java/book/chapter06/Application6_5.java
+++ src/main/java/book/chapter06/Application6_5.java
@@ -29,8 +29,7 @@ public class Application6_5 {
     }
 
     private static boolean isNewEngland(String stateCode) {
-        return Arrays.stream(new String[]{"MA", "CT", "ME", "VT", "NH", "RI"})
-                .anyMatch(code -> code.equals(stateCode));
+        return Arrays.asList(new String[]{"MA", "CT", "ME", "VT", "NH", "RI"}).contains(stateCode);
     }
 }
 

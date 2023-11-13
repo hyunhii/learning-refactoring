707f64d407d0587da776c5758021e0a9e0f2863d
hyunhii <be_hyunny@naver.com>
2023-11-14 00:47:27 +0900

    feat: Chapter08 - '문장 슬라이드하기' 예제 코드2

---
+++ src/main/java/book/chapter08/Application8_6_2.java
@@ -0,0 +1,20 @@
+package book.chapter08;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Application8_6_2 {
+    public static void main(String[] args) {
+        Queue<String> availableResources = new LinkedList<>();
+        Queue<String> allocatedResources = new LinkedList<>();
+        String result;
+
+        if(availableResources.size() == 0) {
+            result = "create result";
+            allocatedResources.add(result);
+        } else {
+            result = availableResources.remove();
+            allocatedResources.add(result);
+        }
+    }
+}

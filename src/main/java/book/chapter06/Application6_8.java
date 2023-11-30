381622a8d6679d6e9bd2c15f00d8a8cf5bab555e
hyunhii <be_hyunny@naver.com>
2023-11-30 17:05:57 +0900

    feat: ‘매개변수 객체만들기’ 예제 코드

---
+++ src/main/java/book/chapter06/Application6_8.java
@@ -0,0 +1,62 @@
+package book.chapter06;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Application6_8 {
+    public static void main(String[] args) {
+        Station station = createSampleStation();
+        OperationPlan operationPlan = new OperationPlan(50, 55);
+
+        List<Reading> alerts =
+                readingsOutsideRange(
+                        station, operationPlan.getTemperatureFloor(), operationPlan.getTemperatureCeiling());
+
+        System.out.println("alerts = " + alerts);
+    }
+
+    public static List<Reading> readingsOutsideRange(Station station, int min, int max) {
+        return station.getReadings().stream()
+                .filter(r -> r.getTemp() < min || r.getTemp() > max)
+                .collect(Collectors.toList());
+    }
+
+    private static Station createSampleStation() {
+        String name = "ZB1";
+        List<Reading> readings =
+                List.of(
+                        new Reading(47, LocalDateTime.of(2016, 11, 10, 9, 10)),
+                        new Reading(53, LocalDateTime.of(2016, 11, 10, 9, 20)),
+                        new Reading(58, LocalDateTime.of(2016, 11, 10, 9, 30)),
+                        new Reading(53, LocalDateTime.of(2016, 11, 10, 9, 40)),
+                        new Reading(51, LocalDateTime.of(2016, 11, 10, 9, 50)));
+
+        return new Station(name, readings);
+    }
+}
+
+@Data
+@AllArgsConstructor
+class Station {
+    private String name;
+    private List<Reading> readings = new ArrayList<>();
+}
+
+@Data
+@AllArgsConstructor
+class Reading {
+    private int temp;
+    private LocalDateTime time;
+}
+
+@Data
+@AllArgsConstructor
+class OperationPlan {
+    private int temperatureFloor;
+    private int temperatureCeiling;
+}

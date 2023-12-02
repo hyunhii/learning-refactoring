bc59e5a993523bea52b00156dd10eeb8faab2f24
hyunhii <be_hyunny@naver.com>
2023-12-02 14:50:32 +0900

    refactor: 반복문 쪼개기 - youngest 임시 변수 질의 함수로 변경

--- src/main/java/book/chapter08/Application8_7.java
+++ src/main/java/book/chapter08/Application8_7.java
@@ -10,15 +10,20 @@ public class Application8_7 {
     public static void main(String[] args) {
         List<Person> persons = getPersonList();
 
+        int youngest = getYoungest(persons);
+
+        System.out.println("youngest = " + youngest);
+        System.out.println("totalSalary = " + getTotalSalary(persons));
+    }
+
+    private static int getYoungest(List<Person> persons) {
         int youngest = persons.isEmpty() ? Integer.MAX_VALUE : persons.get(0).getAge();
         for (Person person : persons) {
             if (person.getAge() < youngest) {
                 youngest = person.getAge();
             }
         }
-
-        System.out.println("youngest = " + youngest);
-        System.out.println("totalSalary = " + getTotalSalary(persons));
+        return youngest;
     }
 
     private static int getTotalSalary(List<Person> persons) {

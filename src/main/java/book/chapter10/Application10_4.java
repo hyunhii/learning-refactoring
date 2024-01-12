617384c45a9a869d3c126bc1970b0277ad67aafc
hyunhii <be_hyunny@naver.com>
2024-01-13 01:08:14 +0900

    refactor: 조건부 로직을 다형성으로 바꾸기 - 노르웨이 파랑 앵무 plumage() 로직 오버라이드

--- src/main/java/book/chapter10/Application10_4.java
+++ src/main/java/book/chapter10/Application10_4.java
@@ -67,7 +67,7 @@ class Bird {
             case "아프리카 제비":
                 return null;
             case "노르웨이 파랑 앵무":
-                return voltage > 100 ? "그을렸다." : "예쁘다.";
+                return null;
             default:
                 return "알 수 없다.";
         }
@@ -112,5 +112,10 @@ class AfricanSwallow extends Bird {
 class NorwegianBlueParrot extends Bird {
     public NorwegianBlueParrot(Bird bird) {
         super(bird);
+    }
+
+    @Override
+    public String plumage() {
+        return voltage > 100 ? "그을렸다." : "예쁘다.";
     }
 }
